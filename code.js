71c57d71f0a99a5220fef8fa9a148ee01b1863c1
himanshi509 <77920739+himanshi509@users.noreply.github.com>
2021-06-27 16:42:44 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,105 @@
+var p5Inst = new p5(null, 'sketch');
+
+window.preload = function () {
+  initMobileControls(p5Inst);
+
+  p5Inst._predefinedSpriteAnimations = {};
+  p5Inst._pauseSpriteAnimationsByDefault = false;
+  var animationListJSON = {"orderedKeys":["a6870703-0124-47f7-acff-dbe905f5014c","5ce44e39-12ac-4a66-88cf-a87a0ed6a180","33841f90-7a53-4346-b956-e51d1961959b"],"propsByKey":{"a6870703-0124-47f7-acff-dbe905f5014c":{"name":"monkey","sourceUrl":null,"frameSize":{"x":560,"y":614},"frameCount":10,"looping":true,"frameDelay":12,"version":"n5ZWrjbwDdUD6fYPFm2OACS5XGODTvy8","loadedFromSource":true,"saved":true,"sourceSize":{"x":1680,"y":1842},"rootRelativePath":"assets/a6870703-0124-47f7-acff-dbe905f5014c.png"},"5ce44e39-12ac-4a66-88cf-a87a0ed6a180":{"name":"Banana","sourceUrl":"assets/v3/animations/0Pmc2UypwJxUUUBBxMOOYmiSvh97BJLRo_BQZbjyEto/5ce44e39-12ac-4a66-88cf-a87a0ed6a180.png","frameSize":{"x":1080,"y":1080},"frameCount":1,"looping":true,"frameDelay":4,"version":"XxSK0GGJ5MD.0tN6_qEp99PLMoYPP9F_","loadedFromSource":true,"saved":true,"sourceSize":{"x":1080,"y":1080},"rootRelativePath":"assets/v3/animations/0Pmc2UypwJxUUUBBxMOOYmiSvh97BJLRo_BQZbjyEto/5ce44e39-12ac-4a66-88cf-a87a0ed6a180.png"},"33841f90-7a53-4346-b956-e51d1961959b":{"name":"Stone","sourceUrl":"assets/v3/animations/0Pmc2UypwJxUUUBBxMOOYmiSvh97BJLRo_BQZbjyEto/33841f90-7a53-4346-b956-e51d1961959b.png","frameSize":{"x":512,"y":512},"frameCount":1,"looping":true,"frameDelay":4,"version":"DHiWuO2pS_kqB9UpFD2rIT1G7Dv5KZlw","loadedFromSource":true,"saved":true,"sourceSize":{"x":512,"y":512},"rootRelativePath":"assets/v3/animations/0Pmc2UypwJxUUUBBxMOOYmiSvh97BJLRo_BQZbjyEto/33841f90-7a53-4346-b956-e51d1961959b.png"}}};
+  var orderedKeys = animationListJSON.orderedKeys;
+  var allAnimationsSingleFrame = false;
+  orderedKeys.forEach(function (key) {
+    var props = animationListJSON.propsByKey[key];
+    var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
+    var image = loadImage(props.rootRelativePath, function () {
+      var spriteSheet = loadSpriteSheet(
+          image,
+          props.frameSize.x,
+          props.frameSize.y,
+          frameCount
+      );
+      p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
+      p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
+      p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
+    });
+  });
+
+  function wrappedExportedCode(stage) {
+    if (stage === 'preload') {
+      if (setup !== window.setup) {
+        window.setup = setup;
+      } else {
+        return;
+      }
+    }
+// -----
+
+var monkey=createSprite(50,330,20,40);
+monkey.setAnimation("monkey");
+monkey.scale=0.2;
+
+var ground=createSprite(200,370,400,10);
+var survivalTime=0;
+var score=0;
+var obstaclesGroup=createGroup();
+var bananasGroup=createGroup();
+function draw() {
+  
+  background(255);
+  if(keyDown("space")){
+    monkey.velocityY=-10;
+  }
+  spawnBananas();
+  spawnObstacles();
+  if(obstaclesGroup.isTouching(monkey)){
+    score=score-1;
+  }
+  if(bananasGroup.isTouching(monkey)){
+    score=score+1;
+  }
+  monkey.collide(ground) ; 
+drawSprites(); 
+survivalTime=Math.ceil(frameCount/frameRate());
+text("Survival Time:"+survivalTime,100,50);
+}
+
+function spawnBananas(){
+  if(World.frameCount% 80===0){
+    var banana=createSprite(400,200,30,10);
+    banana.velocityX=-2;
+    banana.setAnimation("Banana");
+    banana.scale=0.05;
+    banana.y=randomNumber(180,200);
+    bananasGroup.add(banana);
+  }
+}
+
+function spawnObstacles(){
+if(World.frameCount% 100===0){
+    var stone=createSprite(400,350,30,10);
+    stone.velocityX=-2;
+    stone.setAnimation("Stone");
+    stone.scale=0.1;
+  obstaclesGroup.add(stone);
+  }  
+}
+  
+
+// -----
+    try { window.draw = draw; } catch (e) {}
+    switch (stage) {
+      case 'preload':
+        if (preload !== window.preload) { preload(); }
+        break;
+      case 'setup':
+        if (setup !== window.setup) { setup(); }
+        break;
+    }
+  }
+  window.wrappedExportedCode = wrappedExportedCode;
+  wrappedExportedCode('preload');
+};
+
+window.setup = function () {
+  window.wrappedExportedCode('setup');
+};
